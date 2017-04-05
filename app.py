be6a9df606f11021bc66257f10f90a8b8b98fe2e
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-04-05 16:44:07 +0800

    hide

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ def makeWebhookResult(data):
     # print (title)
 
     
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+    #          ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
     speech = "Total Number of Movies Found: " + data.get('total_results')
 
     print("Response:")
