fa26dc4fe09ba30846389837818ea40d06548791
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-04-05 16:32:59 +0800

    remove total result from speech

--- app.py
+++ app.py
@@ -84,36 +84,10 @@ def makeWebhookResult(data):
     print ("RESULT: ")
     print (result)
 
-    # title = result.get('original_title')
-    # print ("TITLE: ")
-    # print (title)
-
-   # query = data.get('query')
-   #  if query is None:
-   #      return {}
-
-   #  result = query.get('results')
-   #  if result is None:
-   #      return {}
-
-   #  channel = result.get('channel')
-   #  if channel is None:
-   #      return {}
-
-   #  item = channel.get('item')
-   #  location = channel.get('location')
-   #  units = channel.get('units')
-   #  if (location is None) or (item is None) or (units is None):
-   #      return {}
-
-   #  condition = item.get('condition')
-   #  if condition is None:
-   #      return {}
-    # print(json.dumps(item, indent=4))
-
+    
     # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
     #          ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-    speech = "Total Number of Movies Found: " + total_results
+    speech = "Total Number of Movies Found: "
 
     print("Response:")
     print(speech)
